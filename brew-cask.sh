172e83bbab12acf5ff275e35317c9c230835be7b
Jesse <jesse@mondorobot.com>
2020-01-06 14:54:13 -0700

    update dotfiles for my specific use cases

--- brew-cask.sh
+++ brew-cask.sh
@@ -11,21 +11,21 @@ brew install caskroom/cask/brew-cask
 # brew tap caskroom/versions
 
 # daily
-brew cask install spectacle
-brew cask install dropbox
-brew cask install gyazo
-brew cask install 1password
-brew cask install rescuetime
+# brew cask install spectacle
+# brew cask install dropbox
+# brew cask install gyazo
+# brew cask install 1password
+# brew cask install rescuetime
 # brew cask install flux
 
 # dev
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install imagealpha
-brew cask install imageoptim
+# brew cask install iterm2
+# brew cask install sublime-text
+# brew cask install imagealpha
+# brew cask install imageoptim
 
 # fun
-brew cask install limechat
+# brew cask install limechat
 # brew cask install miro-video-converter
 # brew cask install horndis               # usb tethering
 
@@ -37,17 +37,17 @@ brew cask install limechat
 # brew cask install torbrowser
 
 # less often
-brew cask install disk-inventory-x
-brew cask install screenflow
-brew cask install vlc
-brew cask install gpgtools
-brew cask install licecap
+# brew cask install disk-inventory-x
+# brew cask install screenflow
+# brew cask install vlc
+# brew cask install gpgtools
+# brew cask install licecap
 # brew cask install utorrent
 
-brew cask install spotify
+# brew cask install spotify
 
-brew tap caskroom/fonts 
-brew cask install font-fira-code
+# brew tap caskroom/fonts
+# brew cask install font-fira-code
 
 
 # Not on cask but I want regardless.
